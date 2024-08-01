c46f5890ecba9bccde66cff7a38a1fbc70a5b336
Adarsh Maurya <adarshmaurya0022@gmail.com>
2024-08-01 21:25:45 +0530

    Currency Convertor first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useCallback, useEffect, useRef } from "react";
+import { useState } from "react"
+import bg from "./Assets/bg.jpg"
+import InputBox from "./components/InputBox";
+
+
+function App() {
+  return (
+    <div style={{ backgroundImage: `url(${bg})` }} className="bg-no-repeat bg-center bg-cover w-full h-screen flex justify-center items-center">
+      <div className="bg-[#ffffff58] backdrop-blur-md border-2 border-[#ffffff70] md:w-[600px] rounded-xl p-4">
+        <div className="relative">
+          <InputBox lable="From" margin="3" />
+          <InputBox lable="To" margin="4" />
+          <button className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-cyan-400 py-1 text-white px-4 rounded-lg border-white border-2">SWAP</button>
+        </div>
+        <button className="bg-cyan-400 transition-all hover:bg-cyan-500 text-white w-full rounded-xl py-3">Convert USD to RUP</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/InputBox.jsx
@@ -0,0 +1,19 @@
+function InputBox({ lable, margin="0" }) {
+    return (
+        <div className={`bg-white flex rounded-xl p-4 w-full mb-${margin}`}>
+            <div className="w-1/2 flex flex-col justify-between">
+                <p className="text-black/40 mb-2">{lable}</p>
+                <input type="text" className="placeholder:text-black outline-0" placeholder="0" id="" />
+            </div>
+            <div className="w-1/2 text-right">
+                <p className="text-black/40 mb-2">Currency Type</p>
+                <select className="outline-0 bg-black/10 rounded-lg p-1 text-sm" name="fruits" id="fruit-select">
+                    <option value="apple">urd</option>
+                    <option value="banana">Banana</option>
+                    <option value="cherry">Cherry</option>
+                </select>
+            </div>
+        </div>
+    );
+}
+export default InputBox;
